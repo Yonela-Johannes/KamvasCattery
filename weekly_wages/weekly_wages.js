2c6fdcf7086d039a6727a666c7356d62dd3710d8
Yonela-Johannes <johannesyonela@gmail.com>
2022-04-22 15:41:56 +0200

    done last tests

--- weekly_wages/weekly_wages.js
+++ weekly_wages/weekly_wages.js
@@ -1,3 +1,23 @@
-function weeklyWage(band, weeklyHours) {
+const weeklyWage = (band, weeklyHours) => {
+    const getWeeklyHours = weeklyHours.split(',')
+    let count = 0
+    let weeklyWage = 0
+    for (let i = 0; i < getWeeklyHours.length; i++) {
+        let sortedWeeklyHours = getWeeklyHours[i]
 
+        if (band == 'A') {
+            count += parseInt(sortedWeeklyHours)
+            weeklyWage = count * 23
+        }
+        else if (band == 'B') {
+            count += parseInt(sortedWeeklyHours)
+            weeklyWage = count * 37
+        }
+        else if (band == 'C') {
+            count += parseInt(sortedWeeklyHours)
+            weeklyWage = count * 51
+        }
+    }
+    return getWeeklyHours.length == 7 ? weeklyWage : "missing day"
 };
+
